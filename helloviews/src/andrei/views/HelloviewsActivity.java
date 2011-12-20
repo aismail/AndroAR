22c429cf63b5d8dd7eedde1d72226db542ee32b2
Petre Andrei <p31andrei (at) gmail.com>
2011-12-21 01:43:08 +0200

    LiniarLayout tutorial - xml design.

---
+++ helloviews/src/andrei/views/HelloviewsActivity.java
@@ -0,0 +1,16 @@
+package andrei.views;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class HelloviewsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
