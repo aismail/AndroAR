331f7eda59f8919535aa4a70888778b2bb5b8851
Petre Andrei <p31andrei (at) gmail.com>
2011-12-27 16:14:11 +0200

    WebView - a mini-browser.

---
+++ HelloWebView/src/andrei/webview/HelloWebViewActivity.java
@@ -0,0 +1,40 @@
+package andrei.webview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class HelloWebViewActivity extends Activity {
+	WebView mWebView;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://google.com");
+        mWebView.setWebViewClient(new HelloWebViewClient());
+    }
+    
+    private class HelloWebViewClient extends WebViewClient {
+    	@Override
+    	public boolean shouldOverrideUrlLoading(WebView view, String url) {
+    		view.loadUrl(url);
+    		return true;
+    	}
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    	if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+    		mWebView.goBack();
+    		return true;
+    	}
+    	return super.onKeyDown(keyCode, event);
+    }
+}
