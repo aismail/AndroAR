82b7edaba0244af7c0fe116869b4ae5081059977
Alexandru Damian <alexdamian1989@gmail.com>
2012-03-02 20:07:45 +0200

    Added a mock implementation for getting the cropped image contents for a particular object

--- AndroARServer/src/com/androar/ImageUtils.java
+++ AndroARServer/src/com/androar/ImageUtils.java
@@ -1,11 +1,20 @@
 package com.androar;
 
+import com.androar.comm.ImageFeaturesProtos.DetectedObject;
 import com.androar.comm.ImageFeaturesProtos.Image;
+import com.androar.comm.ImageFeaturesProtos.ImageContents;
+import com.google.protobuf.ByteString;
 
 public class ImageUtils {
 
 	public static String computeImageHash(Image image) {
 		String image_hash = image.getImage().getImageHash();
 		return image_hash;
+	}
+
+	public static ByteString getCroppedImageContents(ImageContents image,
+			DetectedObject detected_object) {
+		//TODO(alex) implement this
+		return image.getImageContents();
 	}
 }
