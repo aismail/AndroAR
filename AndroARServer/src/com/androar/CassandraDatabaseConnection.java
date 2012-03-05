d6af9d9813e12e3d08945c174415eb6fa2eca11a
Alexandru Damian <alexdamian1989@gmail.com>
2012-03-05 21:16:32 +0200

    Added support for the "SaveImage" query. Indexing by GPS is still not working

--- AndroARServer/src/com/androar/CassandraDatabaseConnection.java
+++ AndroARServer/src/com/androar/CassandraDatabaseConnection.java
@@ -4,25 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import me.prettyprint.cassandra.serializers.BytesArraySerializer;
+import me.prettyprint.cassandra.serializers.IntegerSerializer;
 import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.cassandra.serializers.TypeInferringSerializer;
 import me.prettyprint.cassandra.service.ThriftKsDef;
 import me.prettyprint.hector.api.Cluster;
 import me.prettyprint.hector.api.Keyspace;
 import me.prettyprint.hector.api.beans.HColumn;
 import me.prettyprint.hector.api.ddl.ColumnFamilyDefinition;
+import me.prettyprint.hector.api.ddl.ColumnType;
 import me.prettyprint.hector.api.ddl.KeyspaceDefinition;
 import me.prettyprint.hector.api.factory.HFactory;
 import me.prettyprint.hector.api.mutation.Mutator;
 import me.prettyprint.hector.api.query.ColumnQuery;
 import me.prettyprint.hector.api.query.QueryResult;
+import me.prettyprint.hector.api.query.SubColumnQuery;
+import me.prettyprint.hector.api.query.SuperColumnQuery;
 
 import com.androar.comm.ImageFeaturesProtos.DetectedObject;
 import com.androar.comm.ImageFeaturesProtos.Image;
+import com.androar.comm.ImageFeaturesProtos.ImageContents;
 import com.androar.comm.ImageFeaturesProtos.LocalizationFeatures;
 
 public class CassandraDatabaseConnection implements IDatabaseConnection {
 
-	private StringSerializer string_serializer = StringSerializer.get();
+	private static StringSerializer string_serializer = StringSerializer.get();
+	private static BytesArraySerializer bytearray_serializer = BytesArraySerializer.get();
+	private static IntegerSerializer integer_serializer = IntegerSerializer.get();
 	
 	private String hostname;
 	private int port;
@@ -32,7 +41,7 @@ public class CassandraDatabaseConnection implements IDatabaseConnection {
 	public CassandraDatabaseConnection(String hostname, int port) {
 		this.hostname = hostname;
 		this.port = port;
-		
+
 		// Connect to a cluster
 		cluster = HFactory.getOrCreateCluster(
 				Constants.CASSANDRA_CLUSTER_NAME, hostname + ":" + Integer.toString(port));
@@ -51,12 +60,18 @@ public class CassandraDatabaseConnection implements IDatabaseConnection {
 		ColumnFamilyDefinition image_features_column_family_definition =
 				HFactory.createColumnFamilyDefinition(Constants.CASSANDRA_KEYSPACE,
 						Constants.CASSANDRA_IMAGE_FEATURES_COLUMN_FAMILY);
+		image_features_column_family_definition.setColumnType(ColumnType.STANDARD);
+		ColumnFamilyDefinition image_features_indexed_by_gps_column_family_definition =
+				HFactory.createColumnFamilyDefinition(Constants.CASSANDRA_KEYSPACE,
+						Constants.CASSANDRA_IMAGE_FEATURES_INDEXED_BY_GPS_COLUMN_FAMILY);
+		image_features_indexed_by_gps_column_family_definition.setColumnType(ColumnType.STANDARD);
 		ColumnFamilyDefinition object_associations_column_family_definition =
 				HFactory.createColumnFamilyDefinition(Constants.CASSANDRA_KEYSPACE,
 						Constants.CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY);
-
+		object_associations_column_family_definition.setColumnType(ColumnType.SUPER);
 		List<ColumnFamilyDefinition> all_column_families = new ArrayList<ColumnFamilyDefinition>();
 		all_column_families.add(image_features_column_family_definition);
+		all_column_families.add(image_features_indexed_by_gps_column_family_definition);
 		all_column_families.add(object_associations_column_family_definition);
 		
 		KeyspaceDefinition new_keyspace = HFactory.createKeyspaceDefinition(
@@ -76,14 +91,15 @@ public class CassandraDatabaseConnection implements IDatabaseConnection {
 	@Override
 	public boolean storeImage(Image image) {
 		Mutator<String> mutator = HFactory.createMutator(keyspace_operator, string_serializer);
-		// Store image features for this image:
-		// * all detected objects, in columns "objectX";
-		// * image contents
-		// * localization features (toString() of protocol buffer)
+		// IMAGE_FEATURES column family
+		// * image contents (Column)
+		// * localization features (toString() of protocol buffer) (Column)
 		//   - GPS latitude
 		//   - GPS longitude
-		//   - compass orientation
-		// The row key is the image hash.
+		//   - compass orientation 
+		// * number of detected objects
+		// * all detected objects' ids, in columns "objectX" (Column)
+		// The row key is the image hash / id.
 		String image_hash = ImageUtils.computeImageHash(image);
 		Logging.LOG(10, "Image Hash is: " + image_hash);
 		// Detected Objects
@@ -95,13 +111,18 @@ public class CassandraDatabaseConnection implements IDatabaseConnection {
 					HFactory.createStringColumn("object" + object,
 							detected_object.getName()));
 		}
+		mutator.insert(image_hash,
+				Constants.CASSANDRA_IMAGE_FEATURES_COLUMN_FAMILY, 
+				HFactory.createColumn("num_objects", image.getDetectedObjectsCount(),
+						string_serializer, integer_serializer));
 		// Image Contents
 		Logging.LOG(10, 
-				"Storing image contents: " + image.getImage().getImageContents().toString());
+				"Storing image contents: " + image.getImage().getImageContents().toByteArray());
 		mutator.insert(image_hash,
 				Constants.CASSANDRA_IMAGE_FEATURES_COLUMN_FAMILY,
-				HFactory.createStringColumn("image_contents",
-						image.getImage().getImageContents().toString()));
+				HFactory.createColumn("image_contents",
+						image.getImage().getImageContents().toByteArray(),
+						string_serializer, bytearray_serializer));
 		// Localization Features
 		Logging.LOG(10, 
 				"Storing image features: " + image.getLocalizationFeatures().toString());
@@ -109,35 +130,90 @@ public class CassandraDatabaseConnection implements IDatabaseConnection {
 				Constants.CASSANDRA_IMAGE_FEATURES_COLUMN_FAMILY,
 				HFactory.createStringColumn("localization",
 						image.getLocalizationFeatures().toString()));
-		// Store image hash for each detected object
+		// IMAGE_FEATURES_INDEXED_BY_GPS column family
+		// * image_hash (Column)
+		// The row key is the gps position
+		// TODO(alex): add this here, indexed by gps
+		
+		// OBJECTS_TO_IMAGE_ASSOCIATIONS super column family
+		// Since we can't put both standard columns (such as name, description) and super columns
+		// (such as images that contain the objects (with gps position, cropped image, etc.), we're
+		// using a hack: we're using the "metadata" row key to insert meta data, the "image_index"
+		// row key to store the number of images and "imageX" row key to insert images
+		// * metadata (SuperColumn)
+		//   - object name (Column)
+		//   - object description (Column)
+		// * image_index (SuperColumn)
+		//   - first available id for imageX (Column)
+		// * images containing object, in key imageX (Super Column)
+		//   - image hash / id (Column)
+		//   - cropped image contents (Column)
+		//   - distance to viewer (Column)
+		//   - angle to viewer (Column)
+		//   - inferred GPS position (Column)
+		// The row key is the object hash / id / name.
 		for (int object = 0; object < image.getDetectedObjectsCount(); ++object) {
 			DetectedObject detected_object = image.getDetectedObjects(object);
-			// If the detected object doesn't exist yet, then we should add:
-			// * all its metadata;
-			// * a first_available_image_id tag
+			String object_id = detected_object.getName();
 			int first_unused_image_id;
-			if (!detectedObjectExists(detected_object.getName())) {
-				mutator.insert(detected_object.getName(),
-						Constants.CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY,
-						HFactory.createStringColumn("first_available_image_id", "0"));
+			boolean object_exists = detectedObjectExists(object_id);
+			if (!object_exists) {
+				// If the object doesn't exist, then we should also add the medatada supercolumn
 				first_unused_image_id = 0;
+				ArrayList<HColumn<String, String>> metadata_values =
+						new ArrayList<HColumn<String,String>>();
+				metadata_values.add(HFactory.createStringColumn("name", detected_object.getName()));
+				metadata_values.add(HFactory.createStringColumn("description", ""));
+				mutator.insert(object_id,
+						Constants.CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY,
+						HFactory.createSuperColumn(
+								"metadata", metadata_values, string_serializer, string_serializer,
+								string_serializer));
 			} else {
 				first_unused_image_id = 
 						getDetectedObjectFirstAvailableImageId(detected_object.getName());
+				
 			}
-			// We insert a reference to our image
-			mutator.insert(detected_object.getName(),
+			// Add the image super column
+			// TODO(alex, andrei): add support for using multiple serializers for column values
+			ArrayList image_values = new ArrayList();
+			// Add image_hash = String;
+			image_values.add(HFactory.createStringColumn("image_hash", image_hash));
+			// Add cropped_image_contents = ByteArray;
+			image_values.add(HFactory.createColumn("cropped_image_contents",
+					ImageUtils.getCroppedImageContents(image.getImage(),
+							detected_object).toByteArray(),
+					string_serializer, bytearray_serializer));
+			// Add distance_to_viewer = Integer;
+			image_values.add(HFactory.createColumn("distance_to_viewer",
+					detected_object.getDistanceToViewer(),
+					string_serializer, integer_serializer));
+			// Add angle = Integer;
+			image_values.add(HFactory.createColumn("angle", detected_object.getAngleToViewer(),
+					string_serializer, integer_serializer));
+			// Add inferred_gps_position = String;
+			image_values.add(HFactory.createStringColumn("inferred_gps_position",
+					ImageUtils.inferGPSPosition(image.getLocalizationFeatures(), 
+							detected_object).toString()));
+			mutator.insert(object_id,
 					Constants.CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY,
-					HFactory.createStringColumn(
-							"image_hash" + Integer.toString(first_unused_image_id),
-							image_hash));
+					HFactory.createSuperColumn(
+							"image" + first_unused_image_id, image_values, string_serializer, 
+							string_serializer, TypeInferringSerializer.get()));
+			Logging.LOG(10, "Storing image <" + image_hash + "> under key: " + object_id +
+					", super column: " + "image" + first_unused_image_id);
 			// We change the value of the first available image id column
-			mutator.insert(detected_object.getName(),
+			ArrayList<HColumn<String, Integer>> image_index = 
+					new ArrayList<HColumn<String,Integer>>();
+			image_index.add(HFactory.createColumn("first_available_image_id",
+					first_unused_image_id + 1, string_serializer, integer_serializer));
+			mutator.insert(object_id,
 					Constants.CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY,
-					HFactory.createStringColumn("first_available_image_id",
-							Integer.toString(first_unused_image_id + 1)));
+					HFactory.createSuperColumn("image_index", image_index, 
+							string_serializer, string_serializer, integer_serializer));
 		}
-		return false;
+		Logging.LOG(10, "Done storing image");
+		return true;
 	}
 
 	/*
@@ -151,27 +227,30 @@ public class CassandraDatabaseConnection implements IDatabaseConnection {
 		return null;
 	}
 	
-
-	private int getDetectedObjectFirstAvailableImageId(String object_name) {
-		ColumnQuery<String, String, String> column_query = 
-				HFactory.createStringColumnQuery(keyspace_operator);
-        column_query
-        		.setColumnFamily(Constants.CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY)
-        		.setKey(object_name).setName("first_available_image_id");
-        QueryResult<HColumn<String, String>> result = column_query.execute();
-        return Integer.parseInt(result.get().getValue());
+	private int getDetectedObjectFirstAvailableImageId(String object_id) {
+		SubColumnQuery<String, String, String, Integer> sub_column_query =
+				HFactory.createSubColumnQuery(keyspace_operator, string_serializer,
+						string_serializer, string_serializer, integer_serializer);
+		sub_column_query
+				.setColumnFamily(Constants.CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY)
+				.setKey(object_id)
+				.setSuperColumn("image_index")
+				.setColumn("first_available_image_id");
+        QueryResult<HColumn<String, Integer>> result = sub_column_query.execute();
+        return result.get().getValue();
 	}
 
-	private boolean detectedObjectExists(String object_name) {
-		ColumnQuery<String, String, String> column_query = 
-				HFactory.createStringColumnQuery(keyspace_operator);
-        column_query
-        		.setColumnFamily(Constants.CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY)
-        		.setKey(object_name).setName("first_available_image_id");
-        QueryResult<HColumn<String, String>> result = column_query.execute();
-        Logging.LOG(10, "Object Query (looking for <first_available_image_id> column: \n" + 
-        		result.get().toString());
-        return (result != null);
+	private boolean detectedObjectExists(String object_id) {
+		SubColumnQuery<String, String, String, String> sub_column_query =
+				HFactory.createSubColumnQuery(keyspace_operator, string_serializer,
+						string_serializer, string_serializer, string_serializer);
+		sub_column_query
+				.setColumnFamily(Constants.CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY)
+				.setKey(object_id)
+				.setSuperColumn("image_index")
+				.setColumn("first_available_image_id");
+        QueryResult<HColumn<String, String>> result = sub_column_query.execute();
+        return (result.get() != null);
 	}
 
 }
