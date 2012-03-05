29e8846ff00648fd9b20f1e1d210931b46ba8f9a
Alexandru Damian <alexdamian1989@gmail.com>
2012-03-05 21:09:07 +0200

    Added new Column Family names

--- AndroARServer/src/com/androar/Constants.java
+++ AndroARServer/src/com/androar/Constants.java
@@ -5,10 +5,12 @@ public final class Constants {
 	private Constants() {}
 	
 	public static final String CASSANDRA_CLUSTER_NAME = "Test Cluster";
-	public static final String CASSANDRA_KEYSPACE = "AndroARKeyspace";
+	public static final String CASSANDRA_KEYSPACE = "AndroAR";
 	public static final String CASSANDRA_IMAGE_FEATURES_COLUMN_FAMILY = "image_features";
 	public static final String CASSANDRA_OBJECT_TO_IMAGE_ASSOCIATIONS_COLUMN_FAMILY =
 			"object_associations";
+	public static final String CASSANDRA_IMAGE_FEATURES_INDEXED_BY_GPS_COLUMN_FAMILY =
+			"image_features_gps";
 	public static final int CASSANDRA_REPLICATION_FACTOR = 1;
 	
 	public static final String DATABASE_HOST = "emerald";
