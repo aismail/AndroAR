f4836934587751417b5ba046a8281b402d4b4bbb
Alexandru Damian <alexdamian1989@gmail.com>
2012-03-06 09:50:36 +0200

    Moved mocking stuff to common

--- AndroARServer/src/com/androar/MockClient.java
+++ AndroARServer/src/com/androar/MockClient.java
@@ -14,55 +14,10 @@ import com.androar.comm.ImageFeaturesProtos.ImageContents;
 import com.androar.comm.ImageFeaturesProtos.ObjectBoundingBox;
 import com.androar.comm.ImageFeaturesProtos.DetectedObject.DetectedObjectType;
 import com.androar.comm.ImageFeaturesProtos.ObjectMetadata;
+import com.androar.comm.Mocking;
 import com.google.protobuf.ByteString;
 
 public class MockClient {
-	
-	public static ClientMessage createMockClientMessage(String image_path) 
-			throws FileNotFoundException, IOException {
-		File in_file = new File(image_path);
-        FileInputStream fin = new FileInputStream(in_file);
-        byte file_contents[] = new byte[(int) in_file.length()];
-        fin.read(file_contents);
-        
-        ByteString image_contents = ByteString.copyFrom(file_contents);
-        
-        Image image = Image.newBuilder().
-        	addDetectedObjects(
-        		DetectedObject.newBuilder().
-        		setObjectType(DetectedObjectType.BUILDING).
-        		setId("666").
-        		setBoundingBox(
-        			ObjectBoundingBox.newBuilder().
-        			setTop(0).
-        			setBottom(100).
-        			setLeft(0).
-        			setRight(100).
-        			build()).
-        		setDistanceToViewer(20).
-        		setAngleToViewer(15).
-        		setMetadata(ObjectMetadata.newBuilder().
-        				setName("OBJECT_1").
-        				setDescription("OBJECT_1_DESCRIPTION_LONG")).
-        		build()).
-        	setImage(
-        		ImageContents.newBuilder().
-        		setImageHash("IMAGE_HASH").
-        		setImageContents(image_contents)).
-        	build();
-        
-        ClientMessage client_message = ClientMessage.newBuilder()
-        	.setAuthentificationInfo(
-        		AuthentificationInfo.newBuilder()
-        		.setPhoneId("PHONE_ID")
-        		.setHash("CURRENT_HASH_OF_PHONE_ID")
-        		.build())
-        	.setMessageType(ClientMessageType.IMAGES_TO_STORE)
-        	.addImagesToStore(image)
-        	.build();
-        
-        return client_message;
-	}
 	
 	public static void main(String[] args) {
 		Socket socket;
@@ -82,8 +37,7 @@ public class MockClient {
             // We will read an image stored on the Hard Drive for now, it's path is being passed 
             // through params
             
-            
-            Communication.sendMessage(createMockClientMessage(args[1]), out);
+            Communication.sendMessage(Mocking.createMockClientMessage(args[1]), out);
             
             socket.close();
 		} catch (Exception e) {
