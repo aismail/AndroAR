ed4b6602a363cd5ae3ba5972ac832dbdba4a9386
Alexandru Damian <alexdamian1989@gmail.com>
2012-06-03 15:56:58 +0300

    changes host for local testing

--- AndroAROpenCVGUI/src/com/androar/GUIConstants.java
+++ AndroAROpenCVGUI/src/com/androar/GUIConstants.java
@@ -3,5 +3,5 @@ package com.androar;
 public class GUIConstants {
 	public static int WIDTH = 1200;
 	public static int HEIGHT = 800; 
-	public static String SERVER_HOST = "emerald";
+	public static String SERVER_HOST = "localhost";
 }
