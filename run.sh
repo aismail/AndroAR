b0a4eda68cd6606f932859cc77d822ff3e7afcff
Alexandru Damian <alexdamian1989@gmail.com>
2012-07-08 11:48:15 +0300

    Makes setting up database for testing relatively easy

---
+++ run.sh
@@ -0,0 +1,6 @@
+# Store
+java -jar mock_client.jar localhost AndroARCV/src/test/universitate.jpg tnb AndroARCV/src/test/tnb.jpg intercontinental AndroARCV/src/test/intercontinental.jpg
+java -jar mock_client.jar localhost AndroARCV/src/test/universitate2.jpg intercontinental AndroARCV/src/test/intercontinental2.jpg
+java -jar mock_client.jar localhost AndroARCV/src/test/universitate3.jpg tnb AndroARCV/src/test/tnb2.jpg
+java -jar mock_client.jar localhost AndroARCV/src/test/poli.jpg rectorat AndroARCV/src/test/rectorat.jpg
+# Process
